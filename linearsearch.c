0d08b38bf9c96d932fd0d5ea73e2f68951ec0f60
XBPirate-dev <XBhighseas@outlook.com>
2024-07-15 00:47:04 +0530

    Add files via upload

---
+++ linearsearch.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void main() 
+{
+    int a[50], i, n, search, f = 0;
+    printf("Enter the size of the array \n");
+    scanf("%d", &n);
+    printf("Enter elements into the array \n");
+    for (i = 0; i < n; i++)
+ {
+        scanf("%d", &a[i]);
+    }
+    printf("Enter the search element \n");
+    scanf("%d", &search);
+    for (i = 0; i < n; i++) 
+{
+        if (a[i] == search) 
+{
+            printf("Element found at %d position \n", i);
+            f = 1;
+            break; // To stop the loop once the element is found
+        }
+    }
+    if (f == 0)
+        printf("Element not found\n");
+}
